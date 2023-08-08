1c7a15cf070c4b618d26d689ba4c4e2b30ee7339
sourabh053 <sourabhthakur48@gmail.com>
2023-08-08 10:55:15 +0530

    commit to main

---
+++ app.js
@@ -0,0 +1,24 @@
+let data = [
+    {
+      "category": "Reaction",
+      "score": 80,
+      "icon": "./assets/images/icon-reaction.svg"
+    },
+    {
+      "category": "Memory",
+      "score": 92,
+      "icon": "./assets/images/icon-memory.svg"
+    },
+    {
+      "category": "Verbal",
+      "score": 61,
+      "icon": "./assets/images/icon-verbal.svg"
+    },
+    {
+      "category": "Visual",
+      "score": 72,
+      "icon": "./assets/images/icon-visual.svg"
+    }
+  ]
+
+document.getElementById("rxn").innerText = data[0].category
