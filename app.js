66aafbfb6136269e1366846add05eb6ec58e7b7b
sourabh053 <sourabhthakur48@gmail.com>
2023-08-10 09:48:51 +0530

    ready for deployment

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-let data = [
-    {
-      "category": "Reaction",
-      "score": 80,
-      "icon": "./assets/images/icon-reaction.svg"
-    },
-    {
-      "category": "Memory",
-      "score": 92,
-      "icon": "./assets/images/icon-memory.svg"
-    },
-    {
-      "category": "Verbal",
-      "score": 61,
-      "icon": "./assets/images/icon-verbal.svg"
-    },
-    {
-      "category": "Visual",
-      "score": 72,
-      "icon": "./assets/images/icon-visual.svg"
-    }
-  ]
-  document.getElementById("image").src = data[0].icon
-document.getElementById("rxn").innerText = data[0].category
-document.getElementById("score").innerText = data[0].score+"     "
+fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("rxn").innerText = data[0].category;
+      document.getElementById("score1").innerText = data[0].score+ "     ";
+      document.getElementById("image1").src = data[0].icon;
+
+      document.getElementById("memo").innerText = data[1].category;
+      document.getElementById("score2").innerText = data[1].score+ "     ";
+      document.getElementById("image2").src = data[1].icon;
+
+      document.getElementById("verb").innerText = data[2].category;
+      document.getElementById("score3").innerText = data[2].score+ "     ";
+      document.getElementById("image3").src = data[2].icon;
+
+      document.getElementById("visu").innerText = data[3].category;
+      document.getElementById("score4").innerText = data[3].score+ "     ";
+      document.getElementById("image4").src = data[3].icon;
+    })
